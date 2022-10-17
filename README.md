cca1210e964ad77f3f48dc97a0571da051f3db3f
Canmor Lam <canmor.lam@gmail.com>
2022-10-17 23:36:11 +0800

    Implement breathing light

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # breathing-light
+
+## Steps to build
+
+1. Requirements
+    - CMake, v3.19+
+    - Arm GNU Toolchain, v7+
+      - 11.2 is not recommended because of defect "Illegal instruction": https://bugs.linaro.org/show_bug.cgi?id=5825
+2. Just run CMake to build
+    ```shell
+    mkdir build
+    cmake --toolchain cmake/cortex-m0.cmake -B build
+    cmake --build build --target dist
+    ```
---
+++ board/stm32f072b-disco/startup.s
@@ -0,0 +1,53 @@
+.syntax unified
+.cpu cortex-m0
+.thumb
+
+.section .rodata.vector
+    .word 0x20004000
+    .word main
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
